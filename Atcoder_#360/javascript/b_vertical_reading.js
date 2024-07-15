3f2922cbf6f732eaa8bca037e35bfd0b0e844c0e
Mei Monden <monden.mei.infratop@gmail.com>
2024-07-16 08:02:22 +0900

    [ANS360]

---
+++ Atcoder_#360/javascript/b_vertical_reading.js
@@ -0,0 +1,36 @@
+function Main(input) {
+  input = input.trim().split('\n');
+  const S = input[0];
+  const T = input[1];
+
+  const n = S.length;
+  const t_len = T.length;
+
+  let found = false;
+
+  for (let w = 1; w <= n; w++) {
+    for (let c = 1; c <= w; c++) {
+      let new_string = "";
+      let idx = c - 1;
+
+      while (idx < n) {
+        new_string += S[idx];
+        idx += w;
+      }
+
+      if (new_string === T) {
+        found = true;
+        break;
+      }
+    }
+    if (found) {
+      break;
+    }
+  }
+
+  console.log(found ? "Yes" : "No");
+}
+
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin', 'utf8');
+Main(input);
