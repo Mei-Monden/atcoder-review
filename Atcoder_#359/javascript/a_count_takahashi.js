0b03821c6bb4835868c731863c7f836c0f46f774
Mei Monden <monden.mei.infratop@gmail.com>
2024-07-28 23:47:48 +0900

    fix method name

--- Atcoder_#359/javascript/a_count_takahashi.js
+++ Atcoder_#359/javascript/a_count_takahashi.js
@@ -1,4 +1,4 @@
-function Main(input) {
+function main(input) {
   const lines = input.trim().split('\n');
   const n = parseInt(lines[0]);
   let count = 0;
@@ -15,4 +15,4 @@ function Main(input) {
 
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin', 'utf8');
-Main(input);
+main(input);
